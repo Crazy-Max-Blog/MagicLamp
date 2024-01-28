1543aef07e99b73dd4ededd059094ba3845a4858
CrazyMax <127091629+Crazy-Max-Blog@users.noreply.github.com>
2024-01-28 22:27:17 +0400

    Add files via upload

---
+++ src/MagicRelay_v0.4/btn.h
@@ -0,0 +1,39 @@
+class button {
+  public:
+    button () {}
+    button (byte pin) {
+      _pin = pin;
+      pinMode(_pin, INPUT);
+    }
+    bool click() {
+      bool btnState = digitalRead(_pin);
+      if (!btnState && !_flag && millis() - _tmr >= 100) {
+        _flag = true;
+        _tmr = millis();
+        return true;
+      }
+      if (!btnState && _flag && millis() - _tmr >= 500) {
+        _tmr = millis ();
+        return true;
+      }
+      if (btnState && _flag) {
+        _flag = false;
+        _tmr = millis();
+      }
+      return false;
+    }
+  private:
+    byte _pin;
+    uint32_t _tmr;
+    bool _flag;
+};
+
+button btn;
+
+void btn_begin() {
+  btn=button(sets.devs.btn.pin);
+}
+
+void btn_tick() {
+  if(btn.click()) bb=!bb;
+}
